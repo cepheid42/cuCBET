3a5f702b2e54168436ab4c2927a12963b1ba6a80
Andrew Sexton <andrew.sexton.42@gmail.com>
2022-11-18 12:20:12 -0500

    Made beam initialization using spherical coords. Need to get back to cartesian

--- src/Beams/Beams.cuh
+++ src/Beams/Beams.cuh
@@ -2,58 +2,78 @@
 #define CBET_BEAMS_CUH
 
 #include "../Utilities/Utilities.cuh"
-#include "../Interpolation/Interpolator.cuh"
 #include "Rays.cuh"
 
 inline constexpr uint32_t num_rings = 10;
-inline constexpr uint32_t m = (num_rings + 1) * num_rings;
+inline constexpr float m = static_cast<float>((num_rings + 1) * num_rings);
 
-float calc_intensity(float r, float I0, float w) {
+vec3 spherical_to_cartesian(float r, float phi, float theta) {
+  // Make sure I agree with Wikipedia about Theta == Theta and Phi == Phi
+  auto x = r * std::sin(theta) * std::cos(phi);
+  auto y = r * std::sin(theta) * std::sin(phi);
+  auto z = r * std::cos(theta);
+
+  return {x, y, z};
+}
+
+float calc_intensity(float I0, float r, float w) {
   return I0 * std::exp(-2.0f * SQR(r / w));
 }
 
 struct Beam {
   uint32_t ID;
-  vec3 k_vec;
   float3 origin;
   float radius;
   float intensity;
+  float focal;
   uint32_t nRays;
   Ray* rays;
 
   Beam(const Parameters& params, 
        uint32_t ID, 
        float3 origin, 
-       vec3 kvec, 
-       float r, 
-       float I0, 
+       float radius,
+       float I0,
+       float focal_length,
        uint32_t nrays)
   : ID(ID),
     origin(origin),
-    k_vec(kvec),
-    radius(r),
-    intensity(I),
+    radius(radius),
+    intensity(I0),
+    focal(focal_length),
     nRays(nrays)
   {
     // Initialize rays
     cudaChk(cudaMallocManaged(&rays, nrays * sizeof(Ray)))
     cudaChk(cudaDeviceSynchronize())
 
-    auto rad = radius / num_rings;
     auto nraysf = static_cast<float>(nrays);
 
-    auto width = 0.6f * 2.0f * radius;
+    auto dr = radius / num_rings;
+    auto ds = m * Constants::PI * dr / nraysf;
+
+    auto sigma = 1.7E-4f;
     auto raycount = 0;
-    nvcc
-    for (auto ring = 1; ring <= num_rings; ring++) {
-      auto fring = static_cast<float>(ring);
-      auto num_rays = static_cast<uint32_t>(std::round((2.0f * nraysf * fring) / m));
-      auto dr2 = SQR(fring * rad);
 
-      auto I_ray = calc_intensity(rad, intensity, width);
+    for (auto i = 1; i <= num_rings; i++) {
+      auto ring = static_cast<float>(i);
 
-      for (auto theta = 0; theta < num_rays; theta++) {
-        rays[raycount] = {dim3(0, 0, 0), vec3(0, 0, 0), I_ray};
+      auto num_rays = static_cast<uint32_t>(std::round((2.0f * nraysf * ring) / m));
+
+      auto r = ring * ds;
+      auto theta = std::atan(r / focal);
+
+      auto I_ring = calc_intensity(intensity, r, sigma);
+
+      for (auto j = 0; j < num_rays; j++) {
+        auto arc = static_cast<float>(j);
+        auto phi = arc * ds;
+
+        auto kvec = spherical_to_cartesian(r, phi, theta);
+
+        
+
+        rays[raycount] = Ray{dim3(0, 0, 0), vec3(0, 0, 0), I_ring};
         raycount++;
       }
     }
--- src/main.cu
+++ src/main.cu
@@ -1,5 +1,5 @@
-//#include "Parameters/Parameters.cuh"
-//#include "Beams/Beams.cuh"
+#include "Parameters/Parameters.cuh"
+#include "Beams/Beams.cuh"
 
 //uint32_t* marked  = 200 * nx * ny * nz * sizeof(uint32_t);
 //uint32_t* counter = nx * ny * nz * sizeof(uint32_t);
@@ -15,33 +15,8 @@
 //float* wMult          = 2 * nbeams * nrays * ncrossings * sizeof(float);
 
 //#include "./Utilities/Utilities.cuh"
-#include "./Interpolation/Interpolator.cuh"
 
 int main() {
-  constexpr auto Nx = 101;
-  auto dx = 6.28f / (Nx - 1);
-  auto np = 51;
-  auto dp = 6.28f / float(np - 1);
-
-  auto* xs = new float[np];
-  auto* ys = new float[np];
-
-  for (auto i = 0; i < np; i++) {
-    xs[i] = float(i) * dp;
-    ys[i] = std::sin(xs[i]);
-  }
-
-//  for (auto i = 0; i < np; i++) {
-//    std::cout << xs[i] << ", " << ys[i] << std::endl;
-//  }
-
-  Interpolator<LINEAR, cpu_managed> linterp(xs, ys, np);
-
-  for(auto i = 0; i < Nx; i++) {
-    auto x = linterp(float(i) * dx);
-//    auto x = float(i) * dx;
-//    std::cout << x << ", " << linterp(x) << std::endl;
-  }
 
   return 0;
 }
