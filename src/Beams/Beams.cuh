c2730f37c036b38f194bffa42c7eef130cbe2800
Andrew Sexton <andrew.sexton.42@gmail.com>
2022-11-18 15:40:12 -0500

    working with vectors. added rotation of kvec, working on finding ray origin points

--- src/Beams/Beams.cuh
+++ src/Beams/Beams.cuh
@@ -7,13 +7,33 @@
 inline constexpr uint32_t num_rings = 10;
 inline constexpr float m = static_cast<float>((num_rings + 1) * num_rings);
 
+vec3 polar_to_cartesian(float r, float phi) {
+
+}
+
 vec3 spherical_to_cartesian(float r, float phi, float theta) {
-  // Make sure I agree with Wikipedia about Theta == Theta and Phi == Phi
   auto x = r * std::sin(theta) * std::cos(phi);
   auto y = r * std::sin(theta) * std::sin(phi);
   auto z = r * std::cos(theta);
 
   return {x, y, z};
+}
+
+vec3 cartesian_to_spherical(float x, float y, float z) {
+  auto r = std::sqrt(SQR(x) + SQR(y) + SQR(z));
+  auto theta = std::atan2(std::sqrt(SQR(x) + SQR(y)), z);
+  auto phi = std::atan2(y, x);
+
+  return {r, phi, theta};
+}
+
+vec3 rotate(const vec3& v, const float theta) {
+  auto cos = std::cos(theta);
+  auto sin = std::sin(theta);
+  auto xp = v.x() * cos + v.y() * sin;
+  auto yp = -v.x() * sin + v.y() * cos;
+
+  return {xp, yp, v.z()};
 }
 
 float calc_intensity(float I0, float r, float w) {
@@ -22,7 +42,8 @@ float calc_intensity(float I0, float r, float w) {
 
 struct Beam {
   uint32_t ID;
-  float3 origin;
+  vec3 origin;
+  vec3 k_vec;
   float radius;
   float intensity;
   float focal;
@@ -31,16 +52,18 @@ struct Beam {
 
   Beam(const Parameters& params, 
        uint32_t ID, 
-       float3 origin, 
-       float radius,
+       vec3 o,
+       vec3 k,
+       float r,
        float I0,
-       float focal_length,
+       float f,
        uint32_t nrays)
   : ID(ID),
-    origin(origin),
-    radius(radius),
+    origin(o),
+    k_vec(k),
+    radius(r),
     intensity(I0),
-    focal(focal_length),
+    focal(f),
     nRays(nrays)
   {
     // Initialize rays
@@ -52,6 +75,16 @@ struct Beam {
     auto dr = radius / num_rings;
     auto ds = m * Constants::PI * dr / nraysf;
 
+    auto z_norm = vec3{0.0, 0.0, 1.0};
+
+    // Find rotation angle to align x-y to plane of origin.
+    auto 
+
+    // Find angle for rotating ray vectors to align with the k-vector.
+    // The spherical coords below assume theta aligns along z-axis.
+    auto k_unit = unit_vector(k_vec);
+    auto k_rot_angle = std::acos(dot(k_unit, z_norm) / (k_unit.length()));
+
     auto sigma = 1.7E-4f;
     auto raycount = 0;
 
@@ -60,18 +93,19 @@ struct Beam {
 
       auto num_rays = static_cast<uint32_t>(std::round((2.0f * nraysf * ring) / m));
 
-      auto r = ring * ds;
-      auto theta = std::atan(r / focal);
+      auto R = ring * ds;
+      auto theta = std::atan2(r, focal);
 
-      auto I_ring = calc_intensity(intensity, r, sigma);
+      auto I_ring = calc_intensity(intensity, R, sigma);
 
       for (auto j = 0; j < num_rays; j++) {
         auto arc = static_cast<float>(j);
         auto phi = arc * ds;
 
-        auto kvec = spherical_to_cartesian(r, phi, theta);
+        auto
 
-        
+        auto k_ray = spherical_to_cartesian(r, phi, theta);
+        auto k_ray_rotated = rotate(k_ray, k_rot_angle);
 
         rays[raycount] = Ray{dim3(0, 0, 0), vec3(0, 0, 0), I_ring};
         raycount++;
