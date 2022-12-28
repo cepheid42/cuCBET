6073e347935493ab6f20a5055b5bbda64724e5ea
Andrew Sexton <asexton2@hyrule.ib>
2022-12-28 15:20:42 -0500

    fixed bug in beam init, was using beam distance instead of beam radius

--- src/Beams/Beams.cuh
+++ src/Beams/Beams.cuh
@@ -40,7 +40,7 @@ struct Beam {
 };
 
 void Beam::init_rays() {
-  const float dr = b_dist / num_rings;
+  const float dr = b_radius / num_rings;
   const float a0 = std::sin(Constants::PI / n0);
   const auto beam_loc = b_dist * b_norm;
 
@@ -59,7 +59,7 @@ void Beam::init_rays() {
 
   for (auto i = 1; i <= num_rings; i++) {
     auto r = static_cast<float>(i) * dr;
-    auto n = std::rintf(Constants::PI / std::asin(a0 / static_cast<float>(i)));
+    auto n = std::rint(Constants::PI / std::asin(a0 / static_cast<float>(i)));
 
     for (auto j = 0; j < static_cast<int>(n); j++) {
       float theta = j * (2.0 * Constants::PI) / n;
@@ -79,18 +79,16 @@ void Beam::init_rays() {
 }
 
 void beam_to_csv(Beam& b, const std::string& filename) {
-  std::ofstream file("./" + filename + "_rays.csv");
+  std::ofstream file(filename);
 
-  auto nt = 10;
-  auto dt = 1.0 / static_cast<float>(nt);
+  auto nt = 4;
+  auto dt = 1.0 / static_cast<float>(nt - 1);
 
-  for (auto i = 0; i < b.nRays; i++) {
-    for (auto j = 0; j < nt; j++) {
-      float t = j * dt;
-      
-      Vector3<float> p = b.rays[i].eval(t);
-      file << p << '\n';
-    }
+  for (auto i = 0; i < b.nRays; i++) {      
+    Vector3<float> p1 = b.rays[i].eval(0.0);
+    Vector3<float> p2 = b.rays[i].eval(0.5);
+    Vector3<float> p3 = b.rays[i].eval(1.0);
+    file << p1 << '\n' << p2 << '\n' << p3 << '\n';
   }
 }
 
--- src/main.cu
+++ src/main.cu
@@ -1,5 +1,4 @@
 //#include "Parameters/Parameters.cuh"
-//#include "Beams/Beams.cuh"
 
 //uint32_t* marked  = 200 * nx * ny * nz * sizeof(uint32_t);
 //uint32_t* counter = nx * ny * nz * sizeof(uint32_t);
@@ -14,17 +13,22 @@
 //float* polar_angle    = nbeams * nrays * ncrossings * sizeof(float);
 //float* wMult          = 2 * nbeams * nrays * ncrossings * sizeof(float);
 
-//#include "./Utilities/Utilities.cuh"
-
-#include <type_traits>
-// #include "./Utilities/Vector3.cuh"
+#include "./Utilities/Utilities.cuh"
 #include "./Beams/Beams.cuh"
 
 int main() {
-  vec3 bnorm{-1.0, 0.0, 0.0};
-  Beam* b1 = new Beam(0, bnorm, 1.0, 0.1, 10.0, 1.0);
+  vec3 bnorm1{-1.0, 0.0, 0.0};
+  Beam* b1 = new Beam(0, bnorm1, 1.0, 0.1, 10.0, 1.0);
 
-  beam_to_csv(*b1, "beamtest");
+  vec3 bnorm2{0.0, -1.0, 0.0};
+  Beam* b2 = new Beam(1, bnorm2, 1.0, 0.1, 10.0, 1.0);
+
+  vec3 bnorm3{-0.347252784, -0.782219564,	0.517250479};
+  Beam* b3 = new Beam(2, bnorm3, 1.0, 0.1, 10.0, 1.0);
+
+  beam_to_csv(*b1, "./beam1.csv");
+  beam_to_csv(*b2, "./beam2.csv");
+  beam_to_csv(*b3, "./beam3.csv");
 
   return 0;
 }
