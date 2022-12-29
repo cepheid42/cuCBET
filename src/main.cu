ff994f15fc49d084319fa4097a8ded7f34d6ee5e
Andrew Sexton <andrew.sexton.42@gmail.com>
2022-12-28 20:38:28 -0500

    file reading, hooray

--- src/main.cu
+++ src/main.cu
@@ -1,5 +1,3 @@
-//#include "Parameters/Parameters.cuh"
-
 //uint32_t* marked  = 200 * nx * ny * nz * sizeof(uint32_t);
 //uint32_t* counter = nx * ny * nz * sizeof(uint32_t);
 //float* eden       = nx * ny * nz * sizeof(float);
@@ -13,22 +11,43 @@
 //float* polar_angle    = nbeams * nrays * ncrossings * sizeof(float);
 //float* wMult          = 2 * nbeams * nrays * ncrossings * sizeof(float);
 
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+//#include "Parameters/Parameters.cuh"
 #include "./Utilities/Utilities.cuh"
 #include "./Beams/Beams.cuh"
 
+template<typename T = float>
+std::vector<std::vector<T>> load_beam_normals(const std::string& filename) {
+  std::vector<std::vector<T>> beam_normals;
+
+  std::ifstream file(filename);
+  std::string line;
+  // Read one line at a time into the variable line:
+  while(std::getline(file, line)) {
+      std::vector<T> lineData;
+      std::stringstream lineStream(line);
+      T value;
+
+      while(lineStream >> value) {
+          lineData.push_back(value);
+      }
+      beam_normals.push_back(lineData);
+  }
+  
+  return beam_normals;
+}
+
 int main() {
-  vec3 bnorm1{-1.0, 0.0, 0.0};
-  Beam* b1 = new Beam(0, bnorm1, 1.0, 0.1, 10.0, 1.0);
+  auto beam_normals = load_beam_normals("./beamnorms.csv");
 
-  vec3 bnorm2{0.0, -1.0, 0.0};
-  Beam* b2 = new Beam(1, bnorm2, 1.0, 0.1, 10.0, 1.0);
-
-  vec3 bnorm3{-0.347252784, -0.782219564,	0.517250479};
-  Beam* b3 = new Beam(2, bnorm3, 1.0, 0.1, 10.0, 1.0);
-
-  beam_to_csv(*b1, "./beam1.csv");
-  beam_to_csv(*b2, "./beam2.csv");
-  beam_to_csv(*b3, "./beam3.csv");
+  for (auto bnorm: beam_normals) {
+    std::cout << bnorm << std::endl;
+  }
 
   return 0;
 }
